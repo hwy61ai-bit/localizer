632a06320f20fd4147adb1065ea9a9759435c701
Andrew Arrison <hwy61ai@gmail.com>
2026-03-14 12:46:51 -0400

    Add resvg and sharp as server external packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["sharp", "@resvg/resvg-js"],
   experimental: {
     serverActions: {
       bodySizeLimit: '20mb',
